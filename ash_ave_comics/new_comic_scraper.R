9ffa1e209eee6f9e453e962cee52ccca3d2ec89b
nik-obrien <nikolas.j.obrien@gmail.com>
2022-02-14 17:31:26 -0700

    adding dc logic

--- ash_ave_comics/new_comic_scraper.R
+++ ash_ave_comics/new_comic_scraper.R
@@ -9,18 +9,51 @@
 # and let them know what they have and how much it will cost
 ##################################################################
 
-new_comic_scraper <- function(publisher = NA_character_, content_type = c('Comic Book')){
-  # publisher is a string to determine who we are scraping from; dc, marvel etc #
+new_comic_scraper <- function(publisher = c(NA_character_), content_type = c('Comic Book')){
+  # publisher is a vector string to determine who we are scraping from; dc, marvel etc #
   # content type is a vector or strings such as comic books, graphic novels etc #'
   
-  ##### 1. Import Libraries #####
+  ##### 1. Import Libraries and Initialize Output #####
   library(tidyverse)
   library(rvest)
   library(data.table)
   library(lubridate)
   library(dplyr)
   
+  new_comics <- data.table()
+  
   
   ##### 2. DC Comics #####
+  if(publisher == 'DC'){
+    ##### 2a. Pull the Titles #####
+    # link for dc new comics #
+    dc_link <- 'https://www.dccomics.com/comics?all=1#browse'
+    
+    # read in the html #
+    dc_html <- read_html(dc_link)
+    
+    # extract the new comics from css node #
+    comic_title <- dc_html %>% html_nodes('.slick--optionset--comics-and-graphic-novels a') %>% html_text()
+    
+    # transform into data table and remove blanks #
+    dc_comics <- as.data.table(comic_title)
+    dc_comics <- dc_comics[comic_title != '',]
+    
+    
+    ##### 2b. Pull Each Title's Specific Info #####
+    
+    
+  }
   
+  
+  ##### 3. Marvel Comics #####
+  if(publisher == 'Marvel'){
+    
+  }
+  
+  
+  ##### 4. Image Comics #####
+  if(publisher == 'Image'){
+    
+  }
 }
